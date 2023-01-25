90937b06345924f6f4289cebdc8555dd428a197d
Matt <carr.matthew.lee@gmail.com>
2023-01-24 23:13:20 -0600

    prettier, eslint setup

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Maddy",
+      breed: "Lab/Pit/Good Girl mix",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Socks",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Cooper",
+      breed: "Dachshund/Chihuahua mix",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
